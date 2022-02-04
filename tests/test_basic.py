e14e5cfbe607656b8d8164b1f0354a9d65268298
Mark Veltzer <mark.veltzer@gmail.com>
2022-02-04 09:28:38 +0200

    simplified this project

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,3 +5,5 @@ from add import add
 class TestBasic(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2,2), 4)
+    def test_add_big_numbers(self):
+        self.assertEqual(add(100,100), 200)
