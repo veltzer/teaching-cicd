d368357b8ccdf90bf28bfeeceb96f4545f978187
Mark Veltzer <mark.veltzer@gmail.com>
2022-02-04 12:38:17 +0200

    made tests fail

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,7 +4,7 @@ from add import add
 
 class TestBasic(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2,2), 4)
+        self.assertEqual(add(2,2), 5)
     def test_add_big_numbers(self):
         self.assertEqual(add(100,100), 200)
     print("Hello"                    +"goodbye")
