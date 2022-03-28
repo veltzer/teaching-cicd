1663fa26146459379b81377d2f9b3b04be2b14e1
Mark Veltzer <mark.veltzer@gmail.com>
2022-03-28 11:18:33 +0300

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,6 +4,7 @@ from add import add
 
 class TestBasic(unittest.TestCase):
     def test_add(self):
+        print("I'm testing that 2+2 is 4")
         self.assertEqual(add(2,2), 4)
     def test_add_big_numbers(self):
         self.assertEqual(add(100,100), 200)
