23a294a315334eb0298ddaffd00835b31ca7f8a4
Matan <60192623+matan27500n@users.noreply.github.com>
2023-03-28 13:40:39 +0300

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -8,3 +8,5 @@ class TestBasic(unittest.TestCase):
         self.assertEqual(add(2,2), 4)
     def test_add_big_numbers(self):
         self.assertEqual(add(100,100), 200)
+    def test_add_big_numbers2(self):
+        self.assertEqual(add(100,100), 200)
