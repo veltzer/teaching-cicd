803de1cd00301007764ed58ecffcebd31e955881
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-27 12:38:03 +0300

    no commit message given

---
+++ config/python.py
@@ -0,0 +1,28 @@
+console_scripts = [
+    "pyunique=pyunique.main:main",
+]
+config_requires = [
+    "pyyaml",
+]
+dev_requires = [
+    "pypitools",
+]
+make_requires = [
+    "pyclassifiers",
+    "pydmt",
+    "sphinx",
+    "pymakehelper",
+]
+install_requires = [
+    "pytconf",
+    "pylogconf",
+    "tqdm",
+    "lmdb",
+]
+test_requires = [
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "flake8",
+    "mypy",
+]
